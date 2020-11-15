8e6a80bc8f20c305ce53853c28028ff85539b586
Jae <choiubc@gmail.com>
2020-11-15 00:27:26 -0800

    Finish exceipt 9

--- main.php
+++ main.php
@@ -15,225 +15,110 @@
   Apache server can run it, and you must rename it to have a ".php"
   extension.  You must also change the username and password on the 
   OCILogon below to be your ORACLE username and password -->
-  <?php
-include 'connect.php';
-$conn = OpenCon();
-
-?>
   <html>
     <head>
-        <title>CPSC 304 Social Media</title>
+        <title>CPSC 304 Project - Social Media</title>
     </head>
 
     <body>
         <h2>User Creation (Insertion)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
-            <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
-                Username: <input type="text" name="username"> <br /><br />
-                Password: <input type="text" name="password"> <br /><br />
-                Birthday: <input type="text" name="birthday"> <br /><br />
-                First Name: <input type="text" name="firstName"> <br /><br />
-                Last Name: <input type="text" name="lastName"> <br /><br />
+        <form method="post" action="queries/insertion.php">
+   
+                Username: <input type="text" name="username" required> <br /><br />
+                Password: <input type="password" name="password" required> <br /><br />
+                Gender: <input type="text" name="gender"> <br /><br />
+                Birthday: <input type="date" name="birthday" required> <br /><br />
+                First Name: <input type="text" name="firstName" required> <br /><br />
+                Middle Name: <input type="text" name="middleName"> <br /><br />
+                Last Name: <input type="text" name="lastName" required> <br /><br />
 
-            <input type="submit" value="Submit" name="insertSubmit"></p>
+            <input type="submit" value="Submit"></p>
         </form>
 
         <hr />
 
         <h2>DROP USER (Deletion)</h2>
-        <form method="delete" action="main.php"> <!--refresh page when submitted-->
+        <form method="delete" action="main.php">
             <input type="hidden" id="updateQueryRequest" name="updateQueryRequest">
             User Name: <input type="text" name="oldName"> <br /><br />
 
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+            <input type="submit" value="Submit"></p>
         </form>
 
         <hr />
 
         <h2>Update User Info (Update)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
-            Old Name: <input type="text" name="oldName"> <br /><br />
-            New Name: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+        <form method="post" action="queries/update.php">
+            Old Name: <input type="text" name="oldName" required> <br /><br />
+            New Name: <input type="text" name="newName" required> <br /><br />
+            <input type="submit" value="Submit"></p>
         </form>
 
         <hr />
 
         <h2>Get Users By Birthday! (Selection)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
+        <form method="post" action="main.php">
             From: <input type="text" name="oldName"> <br /><br />
             To: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+            <input type="submit" value="Submit"></p>
         </form>
 
         <hr />
 
-        <h2>Get Comment Details (Projection)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
-            User Name: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+        <h2>Get A User's Comments (Projection)</h2>
+        <form method="post" action="queries/projection.php">
+            User Name: <input type="text" name="username" required> <br /><br />
+            <input type="submit" value="Submit"></p>
             
         </form>
 
         <hr />
 
         <h2>Get Users Who Have An Event On The Date (Join)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
+        <form method="post" action="main.php">
             Date: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+            <input type="submit" value="Submit"></p>
             
         </form>
 
         <hr />
 
         <h2>Get Number Of Involved Chats  (Aggregation with Group by)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
-            Username: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+        <form method="post" action="queries/aggregation-groupby.php">
+            User Name: <input type="text" name="username" required> <br /><br />
+            <input type="submit" value="Submit"></p>
             
         </form>
 
         <hr />
 
         <h2>Get Users who are older than certain age and have posted more than certain number of blogs  (Aggregation with Having)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
+        <form method="post" action="main.php">
             Age: <input type="text" name="oldName"> <br /><br />
             Number Of Posts: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+            <input type="submit" value="Submit"></p>
         
         </form>
 
         <hr />
 
-        <h2>Get age of users where average price of their ads is... (Nested Aggregation with Group)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
-            Price: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+        <h2>Get users where their average price of their ads is minimum over all users (Nested Aggregation with Group)</h2>
+        <form method="get" action="queries/nested-aggregation.php">
+            <input type="submit" value="Check"></p>
         
         </form>
 
         <hr />
 
         <h2>Find Users who registered after certain date and posted their ads with price of (Division)</h2>
-        <form method="post" action="main.php"> <!--refresh page when submitted-->
+        <form method="post" action="main.php">
             Date: <input type="text" name="oldName"> <br /><br />
             Price: <input type="text" name="oldName"> <br /><br />
             Price: <input type="text" name="oldName"> <br /><br />
             Price: <input type="text" name="oldName"> <br /><br />
-            <input type="submit" value="Submit" name="updateSubmit"></p>
+            <input type="submit" value="Submit"></p>
         
         </form>
-
-        <?php
-		//this tells the system that it's no longer just parsing html; it's now parsing PHP
-
-        $success = True; //keep track of errors so it redirects the page only if there are no errors
-        $db_conn = NULL; // edit the login credentials in connectToDB()
-        $show_debug_alert_messages = False; // set to True if you want alerts to show you which methods are being triggered (see how it is used in debugAlertMessage())
-
-        function debugAlertMessage($message) {
-            global $show_debug_alert_messages;
-
-            if ($show_debug_alert_messages) {
-                echo "<script type='text/javascript'>alert('" . $message . "');</script>";
-            }
-        }
-
-        function printResult($result) { //prints results from a select statement
-            echo "<br>Retrieved data from table demoTable:<br>";
-            echo "<table>";
-            echo "<tr><th>ID</th><th>Name</th></tr>";
-
-            while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-                echo "<tr><td>" . $row["NID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]" 
-            }
-
-            echo "</table>";
-        }
-
-        function handleUpdateRequest() {
-            global $db_conn;
-
-            $old_name = $_POST['oldName'];
-            $new_name = $_POST['newName'];
-
-            // you need the wrap the old name and new name values with single quotations
-            executePlainSQL("UPDATE demoTable SET name='" . $new_name . "' WHERE name='" . $old_name . "'");
-            OCICommit($db_conn);
-        }
-
-        function handleResetRequest() {
-            global $db_conn;
-            // Drop old table
-            executePlainSQL("DROP TABLE demoTable");
-
-            // Create new table
-            echo "<br> creating new table <br>";
-            executePlainSQL("CREATE TABLE demoTable (id int PRIMARY KEY, name char(30))");
-            OCICommit($db_conn);
-        }
-
-        function handleInsertRequest() {
-            global $db_conn;
-
-            //Getting the values from user and insert data into the table
-            $tuple = array (
-                ":bind1" => $_POST['insNo'],
-                ":bind2" => $_POST['insName']
-            );
-
-            $alltuples = array (
-                $tuple
-            );
-
-            executeBoundSQL("insert into demoTable values (:bind1, :bind2)", $alltuples);
-            OCICommit($db_conn);
-        }
-
-        function handleCountRequest() {
-            global $db_conn;
-
-            $result = executePlainSQL("SELECT Count(*) FROM demoTable");
-
-            if (($row = oci_fetch_row($result)) != false) {
-                echo "<br> The number of tuples in demoTable: " . $row[0] . "<br>";
-            }
-        }
-
-        // HANDLE ALL POST ROUTES
-	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
-        function handlePOSTRequest() {
-            if (connectToDB()) {
-                if (array_key_exists('resetTablesRequest', $_POST)) {
-                    handleResetRequest();
-                } else if (array_key_exists('updateQueryRequest', $_POST)) {
-                    handleUpdateRequest();
-                } else if (array_key_exists('insertQueryRequest', $_POST)) {
-                    handleInsertRequest();
-                }
-
-                disconnectFromDB();
-            }
-        }
-
-        // HANDLE ALL GET ROUTES
-	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
-        function handleGETRequest() {
-            if (connectToDB()) {
-                if (array_key_exists('countTuples', $_GET)) {
-                    handleCountRequest();
-                }
-
-                disconnectFromDB();
-            }
-        }
-
-		if (isset($_POST['reset']) || isset($_POST['updateSubmit']) || isset($_POST['insertSubmit'])) {
-            handlePOSTRequest();
-        } else if (isset($_GET['countTupleRequest'])) {
-            handleGETRequest();
-        }
-		?>
 	</body>
 </html>
 
---
+++ queries/insertion.php
@@ -0,0 +1,54 @@
+<?php
+    include '../connect.php';
+    include '../main.php';
+    $conn = OpenCon();
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $gender = $_POST['gender'];
+    $birthday = $_POST['birthday'];
+    $firstName = $_POST['firstName'];
+    $middleName = $_POST['middleName'];
+    $lastName = $_POST['lastName'];
+    $creationDate = date("Y-m-d");
+
+    $sql = "INSERT INTO users (
+         username,
+         passwrd,
+         gender,
+         birthday,
+         fname,
+         mname,
+         lname,
+         date_of_registration)
+    VALUES (
+            '$username',
+            $password,
+            '$gender',
+            '$birthday',
+            '$firstName',
+            '$middleName',
+            '$lastName',
+            '$creationDate')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Successfully created account <br /><br />";
+
+        $showAllUsers = "SELECT username, fname, lname FROM users";
+        $res = $conn->query($showAllUsers);
+
+        if ($res->num_rows > 0) {
+            echo "<table><tr><th class='borderclass'>User Name</th><th class='borderclass'>First Name</th><th class='borderclass'>Last Name</th></tr>";
+            while($row = $res->fetch_assoc()) { 
+                echo "<tr><td class='borderclass'>".$row["username"]."</td><td class='borderclass'>".$row["fname"]."</td><td class='borderclass'>".$row["lname"]."</td></tr>";
+            }
+            echo "</table>";
+        }
+    } else {
+        echo "Sorry, something went wrong";
+    } 
+
+
+    CloseCon($conn);
+
+?>
