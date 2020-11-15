dde0a7420ce88c6d034f036ae14680abaa60f21a
Rebecca Chang <rebecca.chang1001@gmail.com>
2020-11-14 18:10:36 -0800

    drop tables sql script created

--- DELETE_DB.sql
+++ DELETE_DB.sql
@@ -1,1 +1,16 @@
 DROP DATABASE CPSC304_Project;
+
+DROP TABLE Users;
+DROP TABLE Requester_Befriends_Requestee;
+DROP TABLE Groups;
+DROP TABLE Post
+DROP TABLE User_Likes_Post;
+DROP TABLE Blog;
+DROP TABLE Ad;
+DROP TABLE User_Joins_Group;
+DROP TABLE Group_Event;
+DROP TABLE Postal_Code;
+DROP TABLE Chat;
+DROP TABLE Message;
+DROP TABLE User_Participate_Chat;
+DROP TABLE Comments;
---
+++ INSERT.sql
@@ -0,0 +1,2 @@
+INSERT INTO USERS VALUES('beastWithDeepVoice', '1234', '2020-01-01', 'Raymond', NULL, 'Ng', '2020-09-07');
+INSERT INTO USERS VALUES('', '', '', '', '', '', '');
