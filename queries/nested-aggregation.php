beda37a08c1a4c0adb791c9c8e0346baeb8a44db
Jae <choiubc@gmail.com>
2020-11-15 15:55:43 -0800

    Update number 9 query

--- queries/nested-aggregation.php
+++ queries/nested-aggregation.php
@@ -2,21 +2,20 @@
     include '../connect.php';
     $conn = OpenCon();
     
-    $sql = "SELECT username, birthday, average
-            FROM (SELECT users.username, birthday, AVG(price) average
-                    FROM ad, users, post 
-                    WHERE users.username = post.username AND post.post_id = ad.post_id
-                    GROUP BY users.username) AS t  
-            WHERE average = (SELECT MIN(average) FROM t)";
+    $sql = "SELECT temp.username, temp.birthday, temp.average, MIN(average)
+            FROM (SELECT u.username, birthday, AVG(price) average
+                    FROM ad a, users u, post p 
+                    WHERE u.username = p.username AND p.post_id = a.post_id
+                    GROUP BY username) AS temp";
 
     $result = $conn->query($sql);
 
     if ($result) {
         echo "List Of Users<br /><br />";
         if ($result->num_rows > 0) {
-            echo "<table><tr><th class='borderclass'>User Name</th><th class='borderclass'>Birthday</th></tr>";
+            echo "<table><tr><th class='borderclass'>User Name</th><th class='borderclass'>Birthday</th><th class='borderclass'>Average Ads Price</th></tr>";
             while($row = $result->fetch_assoc()) { 
-                echo "<tr><td class='borderclass'>".$row["username"]."</td><td class='borderclass'>".$row["birthday"]."</td></tr>";
+                echo "<tr><td class='borderclass'>".$row["username"]."</td><td class='borderclass'>".$row["birthday"]."</td><td class='borderclass'>".$row["average"]."</td></tr>";
             }
             echo "</table>";
         }
